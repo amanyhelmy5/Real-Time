81d13b2d85fc9fa13e582a31bc222c471c712d8a
amany helmy <amany.helmy03@gmail.com>
2018-05-27 21:24:15 +0200

    First commit Message

---
+++ EFGTask/Bootstrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace EFGTask
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+
+        }
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(RealTimeModule.Class1));
+
+        }
+
+    }
+}
---
+++ RealTimeModule/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace RealTimeModule
+{
+    public class Class1 : IModule
+    {
+        private readonly IRegionManager regionManager;
+        public Class1(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+        public void Initialize()
+        {
+             regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.KYC));
+            }
+
+           
+    }
+}
